c84a67139e48f76447ade2dd5b0da2ffdd326ab7
Nguyễn Lý Nhật Phương <57012228+nhatphuong99@users.noreply.github.com>
2021-06-21 15:15:53 +0700

    Update models.py

--- codes/models.py
+++ codes/models.py
@@ -207,6 +207,9 @@ class KGEModel(nn.Module, ABC):
         positive_sample = positive_sample.cuda()
         negative_sample = negative_sample.cuda()
         subsampling_weight = subsampling_weight.cuda()
+        
+        if model.name in ['KG2E_KL', 'KG2E_EL']:
+                        model.normalize_embedding()
 
         # negative scores
         negative_score = model((positive_sample, negative_sample), batch_type=batch_type)
